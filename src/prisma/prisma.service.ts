6921723808174102310495b1a1c97d1a507a2a88
RexJS <carlossuplei@gmail.com>
2025-07-08 16:52:49 -0600

    feat: add prisma, add schema and first migration.

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  // constructor() {
+  //   const url: string = process.env.DATABASE_URL;
+  //   super({
+  //     datasources: {
+  //       db: {
+  //         url,
+  //       },
+  //     },
+  //   });
+  // }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
