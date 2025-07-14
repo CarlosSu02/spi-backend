27adebd77da4620d8eed9dc12f89738483e42be6
RexJS <carlossuplei@gmail.com>
2025-07-14 11:47:23 -0600

    feat(prisma/seed): se agrego la propiedad 'skipDuplicates' para evitar errores al aplicar el seed, nuevo rol admin agregado y agregado el primer  usuario admin.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,16 @@
+import { BadRequestException } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import * as argon from 'argon2';
+
 const prisma = new PrismaClient();
 
 async function main() {
   const roles = await prisma.role.createMany({
     data: [
+      {
+        name: 'ADMIN',
+        description: 'Acceso a todo.',
+      },
       {
         name: 'DIRECCION',
         description: 'Personal de dirección.',
@@ -22,9 +29,36 @@ async function main() {
           'Todos los docentes, estos deben crear el perfil de docente.',
       },
     ],
+    skipDuplicates: true,
   });
 
   console.log({ roles });
+
+  const adminRole = await prisma.role.findFirst({
+    where: {
+      name: 'ADMIN',
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  if (!adminRole) throw new Error('Error: Rol Admin no encontrado.');
+
+  const users = await prisma.user.createMany({
+    data: [
+      {
+        name: 'user1',
+        email: 'admin1@gmail.com',
+        code: '12345',
+        hash: await argon.hash('12345'),
+        roleId: adminRole.id,
+      },
+    ],
+    skipDuplicates: true,
+  });
+
+  console.log({ users });
 }
 
 main()
